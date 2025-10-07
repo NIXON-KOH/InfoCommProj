5e2773180e7559a5f2986dd550e8946172b4c0d0
NIXON KOH <Nixon.kohjy@gmail.com>
2025-10-07 16:31:53 +0800

    Delete TryMeBitch/TryMeBitch directory

--- TryMeBitch/TryMeBitch/TryMeBitch/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TryMeBitch.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
